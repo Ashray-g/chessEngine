f3407ae6bfa8a0a4c7361299eda664f0fcc65ad9
Ocasu <ocasumc@gmail.com>
2021-09-10 10:15:00 -0700

    Added openings theory (50000 games from various pgn files)

--- src/main/java/engine/LoadGames.java
+++ src/main/java/engine/LoadGames.java
@@ -6,25 +6,31 @@ import com.github.bhlangonijr.chesslib.move.MoveList;
 import com.github.bhlangonijr.chesslib.pgn.PgnHolder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class LoadGames {
     static ArrayList<Game> grandMasterGames = new ArrayList<>();
-    static ArrayList<MoveList> grandMasterGamesMoves = new ArrayList<>();
+    public static ArrayList<MoveList> grandMasterGamesMoves = new ArrayList<>();
     public static void load() throws Exception {
-//        for(int i = 1;i<=3;i++){
-//            PgnHolder pgn = new PgnHolder("src/main/java/games/master_games("+i+").pgn");
-//            pgn.loadPgn();
-//            grandMasterGames.addAll(pgn.getGames());
-//        }
         PgnHolder pgn = new PgnHolder("src/main/java/games/Nakamura.pgn");
         PgnHolder pgn2 = new PgnHolder("src/main/java/games/Kasparov.pgn");
+        PgnHolder pgn3 = new PgnHolder("src/main/java/games/Modern.pgn");
+        PgnHolder pgn4 = new PgnHolder("src/main/java/games/SicilianNajdorf6Be2.pgn");
+        PgnHolder pgn5 = new PgnHolder("src/main/java/games/openings.pgn");
         pgn.loadPgn();
         pgn2.loadPgn();
+        pgn3.loadPgn();
+        pgn4.loadPgn();
+        pgn4.loadPgn();
         grandMasterGames.addAll(pgn.getGames());
         grandMasterGames.addAll(pgn2.getGames());
+        grandMasterGames.addAll(pgn3.getGames());
+        grandMasterGames.addAll(pgn4.getGames());
+        grandMasterGames.addAll(pgn5.getGames());
         for(Game g : grandMasterGames){
             grandMasterGamesMoves.add(g.getCurrentMoveList());
         }
+        Collections.shuffle(grandMasterGamesMoves);
         System.out.println(grandMasterGamesMoves.size());
     }
 }
--- src/main/java/lichess/Game.java
+++ src/main/java/lichess/Game.java
@@ -1,9 +1,9 @@
 package lichess;
 
+import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.Square;
 import com.github.bhlangonijr.chesslib.move.Move;
 import com.github.bhlangonijr.chesslib.move.MoveList;
-import com.google.gson.internal.bind.SqlDateTypeAdapter;
 import engine.BoardController;
 import engine.LoadGames;
 import engine.Minimax;
@@ -15,14 +15,15 @@ public class Game {
         Setup.setup();
         LoadGames.load();
 
-        String game = APIControl.createChallenge(1);
-//        String game = APIControl.createChallenge("Sidd444", true);
+//        String game = APIControl.createChallenge(1);
+        String game = APIControl.createChallenge("Sidd444", true);
 
         System.out.println("Id: " + game);
 
         Thread.sleep(10000);
 
         int moves = 0;
+        MoveList gm = LoadGames.grandMasterGamesMoves.get(0);
 
         while(!BoardController.getBoard().isMated()
                 && !BoardController.getBoard().isStaleMate()
@@ -47,12 +48,37 @@ public class Game {
             }
 
             BoardController.printBoard();
-            Move best;
-            if(moves!=0) {
+            Move best = new Move(Square.A1, Square.A2);
+            System.out.println("--------------------------");
+            if(moves == 0) {
+                best = gm.get(0);
+                System.out.println("Grandmaster move");
+            }else if(moves <= 15){
+                boolean f = false;
+                for(MoveList ml : LoadGames.grandMasterGamesMoves){
+                    if(ml.size() >= moves*2) {
+                        Board temp = new Board();
+                        for (int i = 0; i < moves * 2; i++) {
+                            temp.doMove(ml.get(i));
+                        }
+                        if (temp.equals(BoardController.getBoard())) {
+                            best = ml.get(moves * 2);
+                            f = true;
+                            System.out.println(ml);
+                            break;
+                        }
+                    }
+                }
+                if(!f) best = Minimax.nextMove(4, true);
+                else System.out.println("Grandmaster move");
+            }else {
                 best = Minimax.nextMove(4, true);
-            }else{
-                best = new Move(Square.E2, Square.E4);
             }
+//            if(moves!=0) {
+//                best = Minimax.nextMove(4, true);
+//            }else{
+//                best = new Move(Square.E2, Square.E4);
+//            }
 
             BoardController.getBoard().doMove(best);
 
